8713871d0fde399939a0bf787ce5d25e0da6cfbe
Karam OFEISA <61629429+Karam333@users.noreply.github.com>
2024-07-01 22:20:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("Website loaded!");
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Welcome to the Artisanal Goods Store!");
 });
